db979f6925953d6e6a561d368349ae78f38deb90
sarthak78381 <sarthakg2k29@gmail.com>
2022-05-18 18:16:42 +0530

    added bar chart using d3.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Routes, Route, Redirect } from "react-router-dom";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import BarChart from "./Components/BarChart/BarChart";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
-      <Route exact path="/graph" element={<LandingPage />} />
+      <Route exact path="/graph" element={<BarChart />} />
     </Routes>
   );
 }
---
+++ src/Components/BarChart/BarChart.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
+
+import Styles from "./barchart.module.scss";
+
+function BarChart() {
+  const arr = ["number1", "number2", "number3", "number4", "number5"];
+
+  //   const [DUMMY_DATA, set_DUMMY_DATA] = useState(
+  //     arr.map((d, i) => {
+  //       return {
+  //         id: i + 1,
+  //         value: Math.floor(Math.random() * 20 + 5),
+  //         key: d,
+  //       };
+  //     })
+  //     );
+
+  let DUMMY_DATA = arr.map((d, i) => {
+    return {
+      id: i + 1,
+      value: Math.floor(Math.random() * 20 + 5),
+      key: d,
+    };
+  });
+
+  const xScale = d3
+    .scaleBand()
+    .domain(DUMMY_DATA.map((d) => d.key))
+    .rangeRound([0, 550])
+    .padding(0.1);
+  const yScale = d3.scaleLinear().domain([0, 25]).range([450, 0]);
+
+  const updateDummyDataOnCLick = (fn) => {
+    DUMMY_DATA = arr.map((d, i) => {
+      return {
+        id: i + 1,
+        value: Math.floor(Math.random() * 20 + 5),
+        key: d,
+      };
+    });
+    fn();
+  };
+
+  useEffect(() => {
+    const container = d3.select("svg");
+
+    const d3Chart = container
+      .selectAll(".bar")
+      .data(DUMMY_DATA)
+      .enter()
+      .append("rect")
+      .classed(".bar", true)
+      .attr("width", xScale.bandwidth())
+      .attr("height", (data) => 450 - yScale(data.value))
+      .attr("x", (data) => xScale(`${data.key}`))
+      .attr("y", (data) => yScale(data.value));
+
+    window.addEventListener("click", () => {
+      updateDummyDataOnCLick(() => {
+        d3Chart
+          .data(DUMMY_DATA)
+          .attr("height", (data) => 450 - yScale(data.value))
+          .attr("y", (data) => yScale(data.value));
+      });
+    });
+    return () => window.removeEventListener("click", {});
+  }, []);
+
+  return (
+    <div className={Styles.barChart_container}>
+      <svg></svg>
+    </div>
+  );
+}
+
+export default BarChart;
